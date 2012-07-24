0f03d4ef62a3192ac6c457f82b5e8106ca11ee19
Yusuke Sakurai <kerokerokerop@gmail.com>
2012-07-24 14:10:02 +0900

    作業ツリーにTestを追加

---
+++ SheffleTests/SheffleTests.h
@@ -0,0 +1,13 @@
+//
+//  SheffleTests.h
+//  SheffleTests
+//
+//  Created by 桜井 雄介 on 12/07/20.
+//  Copyright (c) 2012年 Kaeru Lab. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface SheffleTests : SenTestCase
+
+@end
---
+++ SheffleTests/SheffleTests.m
@@ -0,0 +1,32 @@
+//
+//  SheffleTests.m
+//  SheffleTests
+//
+//  Created by 桜井 雄介 on 12/07/20.
+//  Copyright (c) 2012年 Kaeru Lab. All rights reserved.
+//
+
+#import "SheffleTests.h"
+
+@implementation SheffleTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in SheffleTests");
+}
+
+@end
